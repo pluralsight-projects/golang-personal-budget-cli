a611fe5f55c0d5fa0016deb89f4b78992bac0370
Apurva Agrawal <ag.apurva11@gmail.com>
2019-12-14 17:27:45 +0530

    module2

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,11 +2,11 @@ package module1
 
 // Budget stores budget information
 type Budget struct {
-	Max float32
-	Items []Item
+	Max 	float32
+	Items 	[]Item
 }
 // Item stores item information
 type Item struct {
 	Description string
-	Price float32
+	Price 		float32
 }
